c57e6f547e8e44bfb7cb27b837139891f0c2c118
Aarush Mehrotra <aarushmehrotra@Aarushs-MacBook-Pro-2.local>
2019-01-19 13:40:29 +0530

    Second commit

--- Semi-Automated Platooning for Efficient Transportation/ViewController.swift
+++ Semi-Automated Platooning for Efficient Transportation/ViewController.swift
@@ -7,14 +7,75 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class MainVC: UIViewController, CLLocationManagerDelegate {
+    
+    var pause = false
+    var locationManager: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    var speed: CLLocationSpeed = CLLocationSpeed()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
+        startLocation = nil
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    }
+    
+
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading)
+    {
+        //   capLabel.text = String(format: "%.4f",newHeading.magneticHeading)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
     }
 
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
 
+    @IBAction func journeyStart(_ sender: Any) {
+        print(speedLabel)
+        updateName()
+    }
+    
+    @IBOutlet weak var speedLabel: UILabel!
+    func updateName() {
+        speedLabel.text = "100"
+    }
+    
 }
+
 
